df254583b93a0848a7fb12d2a61a79de23ca624b
whatthechris <cjordanwong@gmail.com>
2026-05-20 16:54:21 +0200

    Initial Commit

---
+++ backto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  backto
+//
+//  Created by Chris Wong on 2026-05-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ backto/backtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  backtoApp.swift
+//  backto
+//
+//  Created by Chris Wong on 2026-05-20.
+//
+
+import SwiftUI
+
+@main
+struct backtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
